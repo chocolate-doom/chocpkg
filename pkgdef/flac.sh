c377bbdfaf320b2a9f9f0c47fd237c9970b2ac55
Simon Howard <fraggle@soulsphere.org>
2016-02-17 00:30:59 -0500

    Bump libflac version number and disable optimizations.
    Assembly optimizations seem to cause build problems on i386 OS X at
    least. We can make do without them.

--- pkgdef/flac.sh
+++ pkgdef/flac.sh
@@ -1,4 +1,7 @@
-PACKAGE_VERSION=1.2.1
+PACKAGE_VERSION=1.3.1
 PACKAGE_URL=http://www.chocolate-doom.org/depends/${PACKAGE_NAME}-${PACKAGE_VERSION}.tar.gz
 PKGCONFIG_NAME=flac
+
+# Compile problems :(
+PACKAGE_CONFIGURE_OPTS="--disable-asm-optimizations"
 
