fdba92aae9743e2975a2043332ca57e1cce4b120
Simon Howard <fraggle@soulsphere.org>
2016-02-18 02:03:44 -0500

    Fix cross-compile of zlib.
    zlib's configure script is hand-rolled and works differently to the
    usual autotools ones.

--- pkgdef/zlib.sh
+++ pkgdef/zlib.sh
@@ -2,3 +2,11 @@ PACKAGE_VERSION=1.2.8
 PACKAGE_URL=http://zlib.net/zlib-${PACKAGE_VERSION}.tar.gz
 PKGCONFIG_NAME=zlib
 
+# zlib's configure script is hand-rolled and doesn't support the normal
+# --host option because, well, who knows.
+do_build() {
+    CC="$BUILD_HOST-gcc" ./configure --prefix="$PACKAGE_INSTALL_DIR" \
+                                     --static
+    make
+}
+
