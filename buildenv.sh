dcb99e1b11db6e54a6a653d45a52ed8bafb56225
Simon Howard <fraggle@soulsphere.org>
2016-06-15 19:26:20 -0400

    Allow package overrides for default variant.
    By default we build the stable variant but provide a user-editable
    list of packages which will be built at the latest variant instead.
    Rename chocolate-doom-git package to chocolate-doom and have it use
    the latest variant for the current Git build.

--- buildenv.sh
+++ buildenv.sh
@@ -20,6 +20,12 @@ MAKE_OPTS=
 # Uncomment to use more threads for faster builds.
 #MAKE_OPTS=-j4
 
+# By default the stable variant of packages is built unless otherwise
+# requested. If a package name appears inside this array, the "latest"
+# variant of that package (ie. source control HEAD) is built instead.
+LATEST_PACKAGES=(chocolate-doom)
+#LATEST_PACKAGES+=(SDL2 SDL2_image SDL2_mixer SDL2_net)
+
 # On OS X, we must set additional options: build 32-bit binaries, and the
 # target API version.
 if [ $(uname) = "Darwin" ]; then
--- pkgdef/chocolate-doom.sh
+++ pkgdef/chocolate-doom.sh
@@ -1,4 +1,5 @@
 description "Conservative, historically-accurate Doom source port"
 dependencies SDL2 SDL2_mixer SDL2_net SDL2_image
-fetch_git https://github.com/chocolate-doom/chocolate-doom.git sdl2-branch
+variant latest fetch_git \
+    https://github.com/chocolate-doom/chocolate-doom.git sdl2-branch
 build_autotools
