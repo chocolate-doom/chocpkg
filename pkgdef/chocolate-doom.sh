ca06a99365023b69653f9528e0f7ed5280462b87
Simon Howard <fraggle@soulsphere.org>
2016-07-17 22:56:24 -0400

    Remove SDL2_image dependency.
    chocolate-doom sdl2-branch no longer depends on SDL2_image as of commit
    chocolate-doom/chocolate-doom@7215d13b6c4f253, so remove this dependency
    and the SDL2_image package.

--- pkgdef/chocolate-doom.sh
+++ pkgdef/chocolate-doom.sh
@@ -1,5 +1,5 @@
 description "Conservative, historically-accurate Doom source port"
-dependencies SDL2 SDL2_mixer SDL2_net SDL2_image
+dependencies SDL2 SDL2_mixer SDL2_net
 variant latest fetch_git \
     https://github.com/chocolate-doom/chocolate-doom.git sdl2-branch
 build_autotools
