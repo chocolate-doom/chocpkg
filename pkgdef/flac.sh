9a1086d773169a8988aa139d6e837c626aa406c9
Simon Howard <fraggle@soulsphere.org>
2017-01-15 00:40:49 +0000

    Add libogg as flac package dependency.
    It does appear to need this to compile.

--- pkgdef/flac.sh
+++ pkgdef/flac.sh
@@ -1,4 +1,5 @@
 description "Library for encoding/decoding .flac lossless audio files"
+dependencies libogg
 check_pkgconfig flac
 # We use a repacked mirror since flac is released as .xz rather than .gz:
 fetch_download https://www.chocolate-doom.org/depends/flac-1.3.1.tar.gz \
