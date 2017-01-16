09d1177b0106b54f299f2005f5ea831d16656351
Simon Howard <fraggle@soulsphere.org>
2017-01-16 00:55:51 +0000

    Pass -static-libgcc via CC instead of LDFLAGS.
    Using LDFLAGS ought to work, but libtool strips it out when compiling,
    so it doesn't. Instead, set the CC variable so that it is always used.
    This fixes the dependency on the libgcc DLL on Windows.

--- buildenv.sh
+++ buildenv.sh
@@ -31,9 +31,12 @@ if [ $(uname) = "Darwin" ]; then
     MACOSX_DEPLOYMENT_TARGET=10.7
     export LDFLAGS MACOSX_DEPLOYMENT_TARGET
 elif [[ "$BUILD_HOST" =~ mingw ]]; then
-    # MingW builds need the -static-libgcc linker option, otherwise we
-    # will depend on an unnecessary DLL, libgcc_s_sjlj-1.dll:
-    LDFLAGS="-static-libgcc ${LDFLAGS:-}"
+    # MingW builds need the -static-libgcc option, otherwise we
+    # will depend on an unnecessary DLL, libgcc_s_sjlj-1.dll. Note that
+    # this specifically needs to be done via the CC environment variable
+    # rather than CFLAGS/LDFLAGS, otherwise libtool strips it out.
+    CC="${BUILD_HOST}-gcc -static-libgcc"
+    export CC
 else
     # Include $INSTALL_DIR/lib in the list of paths that is searched
     # when looking for DLLs. This allows built binaries to be run
