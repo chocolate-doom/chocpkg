3e5b47e802f6502e8867c806fa3396d36cbb6d22
Simon Howard <fraggle@soulsphere.org>
2017-02-21 22:33:44 -0500

    Fix pkgconfig name for glib.

--- pkgdef/glib.sh
+++ pkgdef/glib.sh
@@ -1,5 +1,5 @@
 description "GNOME glib core utility library"
-check_pkgconfig glib
+check_pkgconfig glib-2.0
 fetch_download https://www.chocolate-doom.org/depends/glib-2.47.6.tar.gz \
                da87bea88da06f60d6e7618c574c67b9fd109ee07fc78074dede473dde8f8196
 dependencies libffi gettext zlib
