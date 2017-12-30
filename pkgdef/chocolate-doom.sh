9e8db546011545584e61468495395a0ef318c8b6
Simon Howard <fraggle@soulsphere.org>
2017-12-30 22:31:51 +0100

    Bump stable version to 3.0.0.

--- pkgdef/chocolate-doom.sh
+++ pkgdef/chocolate-doom.sh
@@ -1,8 +1,8 @@
 description "Conservative, historically-accurate Doom source port"
 dependencies SDL2 SDL2_mixer SDL2_net
 variant stable fetch_download \
-    https://www.chocolate-doom.org/downloads/3.0.0-beta1/chocolate-doom-3.0.0-beta1.tar.gz \
-    62362fbe99aac07a8bb610a30e7318741cd007eb8807500994a25ee488025020
+    https://www.chocolate-doom.org/downloads/3.0.0/chocolate-doom-3.0.0.tar.gz \
+    73aea623930c7d18a7a778eea391e1ddfbe90ad1ac40a91b380afca4b0e1dab8
 variant latest fetch_git \
     https://github.com/chocolate-doom/chocolate-doom.git sdl2-branch
 build_autotools
