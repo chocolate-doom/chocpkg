5891dc3b60ae93f1bade30b294523e257026b150
Simon Howard <fraggle@soulsphere.org>
2017-12-30 23:03:47 +0100

    sdl2-branch has now been merged to master.

--- pkgdef/chocolate-doom.sh
+++ pkgdef/chocolate-doom.sh
@@ -4,5 +4,5 @@ variant stable fetch_download \
     https://www.chocolate-doom.org/downloads/3.0.0/chocolate-doom-3.0.0.tar.gz \
     73aea623930c7d18a7a778eea391e1ddfbe90ad1ac40a91b380afca4b0e1dab8
 variant latest fetch_git \
-    https://github.com/chocolate-doom/chocolate-doom.git sdl2-branch
+    https://github.com/chocolate-doom/chocolate-doom.git
 build_autotools
