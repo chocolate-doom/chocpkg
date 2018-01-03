0013d1623cc2d4d1ce53879eead5bbbe0747f0a6
Simon Howard <fraggle@soulsphere.org>
2018-01-03 23:43:37 +0100

    Update URL for libvorbis package.

--- pkgdef/libvorbis.sh
+++ pkgdef/libvorbis.sh
@@ -1,6 +1,7 @@
 description "Library for decoding Ogg Vorbis codec files"
 dependencies libogg
 check_pkgconfig vorbis
-fetch_download http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.gz \
-               6efbcecdd3e5dfbf090341b485da9d176eb250d893e3eb378c428a2db38301ce
+fetch_download \
+    https://ftp.osuosl.org/pub/xiph/releases/vorbis/libvorbis-1.3.5.tar.gz \
+    6efbcecdd3e5dfbf090341b485da9d176eb250d893e3eb378c428a2db38301ce
 build_autotools
