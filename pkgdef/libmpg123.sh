9279913fd981d9e2735562072bb0849285999915
Fabian Greffrath <fabian@greffrath.com>
2020-07-22 12:15:24 +0200

    Update libmpg123 to v1.26.3

--- pkgdef/libmpg123.sh
+++ pkgdef/libmpg123.sh
@@ -1,7 +1,7 @@
 description "Fast console MPEG Audio Player and decoder library"
 check_library mpg123
 variant stable fetch_download \
-    https://www.mpg123.de/download/mpg123-1.26.1.tar.bz2 \
-    74d6629ab7f3dd9a588b0931528ba7ecfa989a2cad6bf53ffeef9de31b0fe032
+    https://www.mpg123.de/download/mpg123-1.26.3.tar.bz2 \
+    30c998785a898f2846deefc4d17d6e4683a5a550b7eacf6ea506e30a7a736c6e
 build_autotools
 
