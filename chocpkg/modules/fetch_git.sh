42fcac56598af7ff0e6fe0c8b6ee2cbfed3a6f1e
Simon Howard <fraggle@soulsphere.org>
2022-05-10 00:10:22 -0400

    Allow switching between branches and support tags.

--- chocpkg/modules/fetch_git.sh
+++ chocpkg/modules/fetch_git.sh
@@ -9,19 +9,30 @@ fetch_git::init() {
 }
 
 do_fetch() {
-    if [ -e "$PACKAGE_BUILD_DIR/.git" ]; then
-        cd "$PACKAGE_BUILD_DIR"
-        # Assume we're tracking an upstream branch, and to be cautious,
-        # don't ever perform or commit merges. If a merge needs to be made,
-        # let the user resolve the problem themselves.
-        git pull --ff-only || chocpkg::abort \
-            "Failed to cleanly 'git pull' from upstream. You may need to" \
-            "manually resolve merge conflicts with local changes, eg." \
-            "" \
-            "    cd $PACKAGE_BUILD_DIR" \
-            "    git pull"
-    else
+    if [ ! -e "$PACKAGE_BUILD_DIR/.git" ]; then
         git clone -b "$GIT_BRANCH" "$GIT_URL" "$PACKAGE_BUILD_DIR"
+        return
     fi
+
+    cd "$PACKAGE_BUILD_DIR"
+    local current_branch
+    if ! current_branch=$(git symbolic-ref --short HEAD) ||
+       [[ "$current_branch" != "$GIT_BRANCH" ]]; then
+        echo "$PACKAGE_NAME: Switching to '$GIT_BRANCH'..."
+        git checkout "$GIT_BRANCH"
+        return
+    fi
+
+    echo "$PACKAGE_NAME: Fast forwarding branch '$GIT_BRANCH' to latest:"
+
+    # Assume we're tracking an upstream branch, and to be cautious,
+    # don't ever perform or commit merges. If a merge needs to be made,
+    # let the user resolve the problem themselves.
+    git pull --ff-only || chocpkg::abort \
+        "Failed to cleanly 'git pull' from upstream. You may need to" \
+        "manually resolve merge conflicts with local changes, eg." \
+        "" \
+        "    cd $PACKAGE_BUILD_DIR" \
+        "    git pull"
 }
 
