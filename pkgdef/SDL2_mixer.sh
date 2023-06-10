db50578fe179297c058fa723ceba7ba975a3fbb0
Michael Day <contact@michaelcday.com>
2023-06-09 22:24:38 -0400

    Bump SDL_mixer version to 2.6.3

--- pkgdef/SDL2_mixer.sh
+++ pkgdef/SDL2_mixer.sh
@@ -3,10 +3,10 @@ dependencies SDL2
 check_pkgconfig SDL2_mixer
 
 variant stable fetch_download \
-    https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.6.2/SDL2_mixer-2.6.2.tar.gz \
-    8cdea810366decba3c33d32b8071bccd1c309b2499a54946d92b48e6922aa371
+    https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.6.3/SDL2_mixer-2.6.3.tar.gz \
+    7a6ba86a478648ce617e3a5e9277181bc67f7ce9876605eea6affd4a0d6eea8f
 GIT_URL=https://github.com/libsdl-org/SDL_mixer.git
-variant stable_git fetch_git $GIT_URL release-2.6.2
+variant stable_git fetch_git $GIT_URL release-2.6.3
 variant latest fetch_git $GIT_URL SDL2
 
 # Disable dependencies on external libraries for sound file formats:
